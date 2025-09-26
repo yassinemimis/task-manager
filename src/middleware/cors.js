a98b3e4f326c668c87323622e61b19d73bf82c60
yassinemimis <yassinekherbouche88@gmail.com>
2025-09-26 19:20:24 +0100

    cors

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -2,7 +2,7 @@
 import cors from "cors";
 
 const corsOptions = {
-  origin: "http://localhost:3000", // مسموح للـ React
+  origin: "https://task-manager-react-97ebkg4gc-la-casas-projects.vercel.app", // مسموح للـ React
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
